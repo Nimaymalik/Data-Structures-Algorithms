2c167b56b16469a639027a1ae41585b9c52bcfbb
Nimaymalik <nimaymalik21@gmail.com>
2025-01-10 00:16:11 +0530

    Create validPalindrome.java

---
+++ DSA_IMPORTANT/Strings/validPalindrome.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class validPalindrome {
+    public static boolean isPalindrome(String s) {
+        // String ss=s.cona
+        
+        
+    }
+    public static void main(String[] args) {
+        String s=" ";
+        System.out.println(isPalindrome(s));
+
+    }
+    
+}
